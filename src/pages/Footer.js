e6a21c36696b1fa803f70d976e8eefb75b84c6c6
kaaiiy <kyyu@csus.edu>
2023-03-06 15:11:36 -0800

    Initial commit

---
+++ src/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Footer.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+/*Navigation Bar that uses React JS, HTML, CSS, and Bootstrap 5*/
+
+export function Footer() {
+  return (
+    <footer className="footer">
+    <p>© San Joaquin Audubon Society</p>
+    </footer>
+  );
+}
