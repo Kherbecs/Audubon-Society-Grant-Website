d2b6438146377b410ef3db1bb3e7567a2b02d0d1
profundities <85223114+profundities@users.noreply.github.com>
2023-03-15 21:46:10 -0700

    JIRA #8

---
+++ src/pages/AdminPortal.js
@@ -0,0 +1,49 @@
+import react from 'react'
+import '../css/AdminPortal.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export function AdminPortal() {
+    return (
+    <div class="wrapper">
+        <button class="logout">Logout</button>
+        <p class="fs-1">
+            <p class="text-center">Current Applicants</p>
+            <p class="text-end">
+                <p class="fs-6">
+                </p>
+            </p>
+        </p>
+        <div class="row g-2">
+            <div class="col-md">
+                <div class="form-floating">
+                    <select class="form-select" id="floatingSelect" aria-label="Filter drop down menu">
+                        <option selected>Filter by grant, date, read/unread</option>
+                        <option value="grant">grant 1</option>
+                        <option value="grant2">grant 2</option>
+                        <option value="date">date</option>
+                        <option value="read">read</option>
+                        <option value="unread">unread</option>
+                    </select>
+                </div>
+            </div>
+        
+            <div class="col-md">
+                <div class="mb-3">
+                    <div class="searchField">
+                        <input type="name" class="form-control" id="searchBar" placeholder="Search for applicants"></input> 
+                        <button class="search">Search</button>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+        <div class="submissions-box">
+                <button class="sub1">Submission</button>
+                <button class="sub2">Submission</button>
+                <button class="sub3">Submission</button>
+            </div>
+    </div>
+    )
+  }
