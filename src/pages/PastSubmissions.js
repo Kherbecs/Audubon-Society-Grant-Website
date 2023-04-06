f77b0636e834c83a46b8fa2d6ec74f0459b49ee1
Jason Alvarado <jasonalvarado410@gmail.com>
2023-04-06 06:08:43 +0000

    JIRA #109

--- src/pages/PastSubmissions.js
+++ src/pages/PastSubmissions.js
@@ -4,38 +4,49 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function PastSubmissions() {
     return (
-        <div class="flex-wrap">
+        <div class="flex-wrap page-content">
             <div class="col">
                 <a class="btn btn-nav btn-sm btn-success back-to-grants" href="/grantselection">Back to Grants</a>
             </div>
-            
+
             <div class="mx-auto text-center page-title">
-                <div class="row">
-                    <div class="container">
-                        <h1>Past Submissions</h1>
-                    </div>
-                </div>
+                <h1 class="past-submissions-title">Past Submissions</h1>
             </div>
 
-            <div class="container">
+            <div class="container container-content">
                 <div class="col-8 card submission-body">
+                    
                     <div class="hstack application-headers p-3">
                             <h4>Applications</h4>
                             <h4>Status</h4>
                     </div>
-                    <div class="card-body row main-card">
-                    <div class="vstack col-6">
-                            <button class="btn btn-success m-2 applications">Bird Watching Grant</button>
-                            <button class="btn btn-success m-2 applications">Bird Watching Grant 2</button>
-                            <button class="btn btn-success m-2 applications">Test</button>
-                            <button class="btn btn-success m-2 applications">Environmental Education and Conservation Youth Scholarship and Birding Summer Camp </button>
+
+                    <div class="card-body row main-card desktop-apps">
+                        <div class="vstack col-6">
+                            <a href="/subappform" class="btn btn-success m-2 applications" role="button">Bird Watching Grant</a>
+                            <a href="#link" class="btn btn-success m-2 applications" role="button">Bird Watching Grant 2</a>
+                            <a href="#link" class="btn btn-success m-2 applications" role="button">Test</a>
+                            <a href="#link" class="btn btn-success m-2 applications" role="button">Environmental Education and Conservation Youth Scholarship and Birding Summer Camp</a>
                         </div>
 
-                        <div class="vstack col-4 application-status">
-                            <div class="btn btn m-2 disabled">Review in Progress</div>
-                            <div class="btn m-2 disabled">Review in Progress</div>
-                            <div class="btn m-2 disabled">Review in Progress</div>
-                            <div class="btn m-2 disabled">Review in Progress</div>
+                        <div class="vstack col-4">
+                            <div class="btn btn m-2 disabled app-status">Review in Progress</div>
+                            <div class="btn m-2 disabled app-status">Review in Progress</div>
+                            <div class="btn m-2 disabled app-status">Review in Progress</div>
+                            <div class="btn m-2 disabled app-status">Review in Progress</div>
+                        </div>
+                    </div>
+
+                    <div class="card-body row main-card mx-auto mobile-apps btn-toolbar">
+                        <div class="vstack col-6">
+                            <a href="#link" class="btn btn-success m-6 applications" role="button">Bird Watching Grant</a>
+                            <div class="btn btn m-2 disabled app-status mb-5">Review in Progress</div>
+                            <a href="#link" class="btn btn-success m-6 applications" role="button">Bird Watching Grant 2</a>
+                            <div class="btn btn m-2 disabled app-status mb-5">Review in Progress</div>
+                            <a href="#link" class="btn btn-success m-6 applications" role="button">Test</a>
+                            <div class="btn btn m-2 disabled app-status mb-5">Review in Progress</div>
+                            <a href="#link" class="btn btn-success m-6 applications" role="button">Environmental Education and Conservation Youth Scholarship and Birding Summer Camp</a>
+                            <div class="btn btn m-2 disabled app-status mb-5">Review in Progress</div>
                         </div>
                     </div>
                 </div>
