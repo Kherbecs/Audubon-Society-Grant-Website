6122f4f43b4c3827dfa41b17407fc54f0c3e51f5
castilan <53244903+castilan@users.noreply.github.com>
2023-04-05 23:19:32 -0700

    JIRA #107

--- src/pages/GrantSelection.js
+++ src/pages/GrantSelection.js
@@ -2,69 +2,60 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/GrantSelection.css'
 
-
-
 export function GrantSelection(){
     return(
         
         <div className="GrantSelection">
             <div className="SubmissionsButton">
-                <button className="Submissions" class="btn btn-dark">
-                    <text><a href="/pastsubmissions">Submissions</a></text>
-                </button>
+                    <text><a href="/pastsubmissions" className="SubmissionText" 
+                    class="btn btn-dark">Submissions</a></text>
             </div>
 
-            <div class="header">
-                <h1>San Joaquin Audubon Society Grants</h1>
+            <div className="GrantScholarshipHeader" header>
+                <h1>Grants and Scholarships</h1>
             </div>       
             
-            <div className="EnvironmentalEducation-Conservation" class="card">    
+            <div className="GrantScholarshipCard" card>    
                 
                 <div className="TopPhoto">
-                    <img src="http://www.sanjoaquinaudubon.org/wp-content/uploads/2022/03/WaldoHoltPic1.jpg" alt="Card cap"></img>
+                    <img className="img_1"src="http://www.sanjoaquinaudubon.org/wp-content/uploads/2022/03/WaldoHoltPic1.jpg" alt="Card cap"></img>
                 </div>  
-                    <div class="card-header">
+                    <div className= "GrantHeader" card-header>
                         <h3>Environmental Education and Conservation</h3>
                     </div>
-                    <div class="card-body">
-                        <div className="infoCollapse" class="card-body">
+                    <div className="BottomHalf" card-body>
+                        <div className="GrantScholarshipInfo" class="card-body">
                                 Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla 
                                 assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt 
                                 sapiente ea proident. Leggings occaecat craft beer farm-to-table, 
                                 raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                         </div>
-                        <div class="container">
-                            <button className="apply" class="btn btn-success">
-                                <text><a href="/applicationformpage">Apply</a></text>
-                            </button>                       
+                        <div className="ApplyButton" container>
+                            <text><a href="/applicationformpage" class="btn btn-success">Apply</a></text>                
                         </div>
                     </div>
-
-            </div>
+                    </div>
             
-            <div className="YouthScholarship-BirdingSummerCamp" class="card">
+            <div className="GrantScholarshipCard" card>
                 <div className="TopPhoto_2">
-               
                     <img className= "img_2" 
                         src="https://www.sanjoaquinaudubon.org/wp-content/uploads/2019/12/1.png" 
                         alt="BlueBird"
                         ></img>
-      
                 </div>  
-                <div class="card-header">
-                    <h3>Youth Scholarship and Birding Summer Camp</h3>
+
+                <div className="ScholarshipHeader" card-header>
+                    <h3>Steve Stocking Youth Environmental Scholarship</h3>
                 </div>
-                    <div class="card-body">
-                        <div className="infoCollapse" class="card-body">
+                    <div className="BottomHalf" card-body>
+                        <div className="GrantScholarshipInfo" class="card-body">
                         Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla 
                         assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt 
                         sapiente ea proident. Leggings occaecat craft beer farm-to-table, 
                         raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                     </div>
-                        <div class="container">
-                            <button className="apply" class="btn btn-success">
-                                <text><a href="/applicationformpage">Apply</a></text> 
-                            </button> 
+                        <div className="ApplyButton" container>
+                            <text><a href="/applicationformpage" class="btn btn-success">Apply</a></text> 
                         </div>
 
                     </div>
@@ -75,4 +66,3 @@ export function GrantSelection(){
     );
 
 }
-
