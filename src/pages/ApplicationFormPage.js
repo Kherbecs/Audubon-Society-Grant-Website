8085fcb2c92a30181e9548b1297abd823373c146
Peter <peter@tmetz.net>
2023-04-19 22:36:32 -0700

    JIRA #163, #166

--- src/pages/ApplicationFormPage.js
+++ src/pages/ApplicationFormPage.js
@@ -7,50 +7,100 @@ export function ApplicationFormPage() {
         <div className = "wrapper-appform">
             <div className = "form-appform">
                 <div className = "grantTitle-appform">
-                    <label for = "title-appform" class = "title-appform">Generic Grant Title</label>
+                    <label for = "title-appform" class = "title-appform">Steve Stocking Youth Environmental Scholarship</label>
                 </div>
                 <div className = "questionSection-appform">
+                    <div className = "q4-appform">
+                        <label for="question4Text" class="form-label-appform4">In the text boxes below, please type your name, address, home phone, email, birth date (mm/dd/yy), and your city, state, and ZIP code.</label>
+                        <div className = "q4Wrapper-appform">
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "fname">First Name</label>
+                                    <input type = "text" class = "form-control" placeholder = "First Name" aria-label= "First Name"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "lname">Last Name</label>
+                                    <input type = "text" class = "form-control" placeholder = "Last Name" aria-label= "Last Name"></input>
+                                </div>
+                            </div>
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "birthday">Birth Date (mm/dd/yy)</label>
+                                    <input type = "text" class = "form-control" placeholder = "Birth Date (mm/dd/yy)" aria-label = "Birth Date (mm/dd/yy)"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "email">Email</label>
+                                    <input type = "text" class = "form-control" placeholder = "Email" aria-label = "Email"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "homephone">Home Phone</label>
+                                    <input type = "text" class = "form-control" placeholder = "Home Phone" aria-label = "Home Phone"></input>
+                                </div>
+                            </div>
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "address">Address</label>
+                                    <input type = "text" class = "form-control" placeholder = "Address" aria-label = "Address"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "city">City</label>
+                                    <input type = "text" class = "form-control" placeholder = "City" aria-label = "City"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "state">State</label>
+                                    <input type = "text" class = "form-control" placeholder = "State" aria-label = "State"></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "zip">ZIP Code</label>
+                                    <input type = "text" class = "form-control" placeholder = "ZIP Code" aria-label = "ZIP Code"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className = "q1-appform">
-                        <label for="question1Text" class="form-label-appform">This is question 1.</label>
-                        <textarea type="answer" class="form-control" id="inputAnswer1" aria-describedby="answerHelp" rows = "1"></textarea>
-                        <div id="questionHelp" class="form-text">Please type your answer in the box above.</div>
+                        <label for="question1Text" class="form-label-appform1">Are you, or a parent or guardian, a member of the San Joaquin Audubon Society?</label>
+                        <div className = "Q1Selection-appform">
+                            <select className = "q1select-appform" id = "q1select-appform">
+                                <option value = "Yes">Yes</option>
+                                <option value = "No">No</option>
+                            </select>
+                        </div>
                     </div>
                     <div className = "q2-appform">
-                        <label for="question2Text" class="form-label-appform">This is question 2.</label>
-                        <textarea type="answer" class="form-control" id="inputAnswer2" aria-describedby="answerHelp" rows = "1"></textarea>
-                        <div id="questionHelp" class="form-text">Please type your answer in the box above.</div>
+                        <label for="question2Text" class="form-label-appform2">Do you live in San Joaquin County?</label>
+                        <div className = "Q2Selection-appform">
+                            <select className = "q2select-appform" id = "q1select-appform">
+                                <option value = "Yes">Yes</option>
+                                <option value = "No">No</option>
+                            </select>
+                        </div>
                     </div>
                     <div className = "q3-appform">
-                        <label for="question3Text" class="form-label-appform">This is question 3.</label>
+                        <label for="question3Text" class="form-label-appform3">Which camp or program do you want to attend?</label>
                         <textarea type="answer" class="form-control" id="inputAnswer3" aria-describedby="answerHelp" rows = "1"></textarea>
                         <div id="questionHelp" class="form-text">Please type your answer in the box above.</div>
                     </div>
-                    <div className = "q4-appform">
-                        <label for="question4Text" class="form-label-appform">This is question 4.</label>
-                        <textarea type="answer" class="form-control" id="inputAnswer4" aria-describedby="answerHelp" rows = "1"></textarea>
-                        <div id="questionHelp" class="form-text">Please type your answer in the box above.</div>
-                    </div>
                     <div className = "qLast-appform">
-                        <label for="qLastText" class="form-label-appform">Do you have any feedback for us?</label>
+                        <label for="qLastText" class="form-label-appformLast">Do you have any feedback or additional comments to make?</label>
                         <textarea type="answer" class="form-control" id="inputAnswerLast" aria-describedby="answerHelp" rows = "1"></textarea>
                         <div id="questionHelp" class="form-text">Please type your answer in the box above.</div>
                     </div>
+                    <div className = "q5-appform">
+                        <label for="question5Text" class="form-label-appform5">Please submit a letter of recommendation from a teacher, parent, or adult friend that should cover what they know about your interest in learning about the environment, nature, or birds. Please use the button below.</label>
+                    </div>
                     <div className = "uploadButtonLetterOfRec-appform">
-                        <div className = "uploadButtonTextDiv-appform">
-                            <label for = "uploadButtonLetterText-appform" id = "form-label-appform">Please use the button below to submit your letter of recommendation.</label>
-                        </div>
                         <input type = "file" id = "letterFile" name = "letter"></input>
                     </div>
+                    <div className = "q6-appform">
+                        <label for="question6Text" class="form-label-appform6">Please also submit a personal essay that states the importance of attending an environmental, nature, or birding camp or program to you. The essay should not be more than 2 pages long. Do you try to teach others about nature or birds? Do you draw trees, plants, birds, or other animals in nature you see? We want to know about your interest in the environment and nature. Please use the button below.</label>
+                    </div>
                     <div className = "uploadButtonEssay-appform">
-                        <div className = "uploadButtonTextDiv-appform">
-                            <label for = "uploadButtonLetterText-appform" id = "form-label-appform">Please use the button below to submit your essay.</label>
-                        </div>
                         <input type = "file" id = "essayFile" name = "essay"></input>
                     </div>
                 </div>
                 <div className = "buttonWrapper1-appform">
                     <div class="text-center">
-                        <button class = "button1-appform">Submit</button>
+                        <button type="submit" className="btn btn-lr btn-success submitbutton-appform">Submit</button>
                     </div>
                 </div>
                 <div className = "buttonWrapper2-appform" >
--- src/pages/SubAppForm.js
+++ src/pages/SubAppForm.js
@@ -7,48 +7,98 @@ export function SubAppForm() {
         <div className = "wrapper-subappform">
             <div className = "form-subappform">
                 <div className = "grantTitle-subappform">
-                    <label for = "title-subappform" class = "title-subappform">Generic Grant Title</label>
+                    <label for = "title-subappform" class = "title-subappform">Steve Stocking Youth Environmental Scholarship</label>
                 </div>
                 <div className = "questionSection-subappform">
+                    <div className = "basicInfo-subappform">
+                        <label for="basicInfoText" class="form-label-subappform1">In the text boxes below, please type your name, address, home phone, email, birth date (mm/dd/yy), and your city, state, and ZIP code.</label>
+                        <div className = "basicInfoWrapper-subappform">
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "fname">First Name</label>
+                                    <input type = "text" value = "John" class = "form-control user-info-field" placeholder = "First Name" aria-label= "First Name" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "lname">Last Name</label>
+                                    <input type = "text" value = "Smith" class = "form-control user-info-field" placeholder = "Last Name" aria-label= "Last Name" readOnly></input>
+                                </div>
+                            </div>
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "birthday">Birth Date (mm/dd/yy)</label>
+                                    <input type = "text" value = "04/19/1993" class = "form-control user-info-field" placeholder = "Birth Date (mm/dd/yy)" aria-label = "Birth Date (mm/dd/yy)" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "email">Email</label>
+                                    <input type = "text" value = "JohnSmith@gmail.com" class = "form-control user-info-field" placeholder = "Email" aria-label = "Email" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "homephone">Home Phone</label>
+                                    <input type = "text" value = "(916) 501-7088" class = "form-control user-info-field" placeholder = "Home Phone" aria-label = "Home Phone" readOnly></input>
+                                </div>
+                            </div>
+                            <div class = "row g-3 row-appform">
+                                <div class = "col-md">
+                                    <label for = "address">Address</label>
+                                    <input type = "text" value = "6801 Shiner Street" class = "form-control user-info-field" placeholder = "Address" aria-label = "Address" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "city">City</label>
+                                    <input type = "text" value = "Sacramento" class = "form-control user-info-field" placeholder = "City" aria-label = "City" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "state">State</label>
+                                    <input type = "text" value = "CA" class = "form-control user-info-field" placeholder = "State" aria-label = "State" readOnly></input>
+                                </div>
+                                <div class = "col-md">
+                                    <label for = "zip">ZIP Code</label>
+                                    <input type = "text" value = "95828" class = "form-control user-info-field" placeholder = "ZIP Code" aria-label = "ZIP Code" readOnly></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className = "q1-subappform">
-                        <label for="qText" class="form-label-subappform">This is question 1.</label>
-                        <div className = "answerarea-subappform">
-                            <p className = "qanswer-subappform">This is the answer to question 1.</p>
+                        <label for="question1Text" class="form-label-subappform2">Are you, or a parent or guardian, a member of the San Joaquin Audubon Society?</label>
+                        <div className = "Q1Selection-subappform">
+                            <select className = "q1select-subappform" id = "q1select-subappform" disabled>
+                                <option value = "Yes">Yes</option>
+                            </select>
                         </div>
                     </div>
                     <div className = "q2-subappform">
-                        <label for="qText" class="form-label-subappform">This is question 2.</label>
-                        <div className = "answerarea-subappform">
-                            <p className = "qanswer-subappform">This is the answer to question 2. This specific answer is longer to see what more text would look like.</p>
-                        </div>
+                        <label for="question2Text" class="form-label-subappform2">Do you live in San Joaquin County?</label>
+                        <div className = "Q1Selection-subappform">
+                            <select className = "q2select-subappform" id = "q1select-subappform" disabled>
+                                <option value = "No">No</option>
+                            </select>
+                        </div>                        
                     </div>
                     <div className = "q3-subappform">
-                        <label for="qText" class="form-label-subappform">This is question 3.</label>
-                        <div className = "answerarea-subappform">
-                            <p className = "qanswer-subappform">This is the answer to question 3. This specific answer is much longer to see what more text would look like.
-                            This answer is even longer so that we can see what multiple lines would look like.</p>
-                        </div>
+                        <label for="question3Text" class="form-label-subappform2">Which camp or program do you want to attend?</label>
+                        <div class="wrapper-user-answer">
+                            <textarea class="form-control" id="textAreaExample" rows="4" 
+                                value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam eleifend mi in nulla posuere sollicitudin." readOnly>
+                            </textarea>
+                        </div>                         
                     </div>
                     <div className = "q4-subappform">
-                        <label for="qText" class="form-label-subappform">This is question 4.</label>
-                        <div className = "answerarea-subappform">
-                            <p className = "qanswer-subappform">This is the answer to question 4. Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
+                        <label for="question4Text" class="form-label-subappform2">Do you have any feedback for us?</label>
+                        <div class="wrapper-user-answer">
+                        <textarea class="form-control" id="textAreaExample" rows="4" 
+                            value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor eget dolor morbi non arcu risus quis. Enim praesent elementum facilisis leo vel. Ultricies leo integer malesuada nunc vel." readOnly>
+                        </textarea>       
+                        </div> 
                     </div>
-                    <div className = "qLast-subappform">
-                        <label for="qTextText" class="form-label-subappform">Do you have any feedback for us?</label>
-                        <div className = "answerarea-subappform"> 
-                            <p className = "qanswer-subappform">This is the feedback.</p>
-                        </div>
+                    <div className = "q5-subappform">
+                        <label for="question5Text" class="form-label-appform5">Please submit a letter of recommendation from a teacher, parent, or adult friend that should cover what they know about your interest in learning about the environment, nature, or birds.</label>
                     </div>
                     <div className = "uploadButtonLetterOfRec-subappform">
                         <div className = "uploadButtonTextDiv-subappform">
                             <label for = "uploadButtonLetterText-subappform" id = "form-label">SAMPLE_LETTER_OF_REC.pdf</label>
                         </div>
+                    </div>
+                    <div className = "q6-subappform">
+                        <label for="question6Text" class="form-label-appform6">Please also submit a personal essay that states the importance of attending an environmental, nature, or birding camp or program to you. The essay should not be more than 2 pages long. Do you try to teach others about nature or birds? Do you draw trees, plants, birds, or other animals in nature you see? We want to know about your interest in the environment and nature.</label>
                     </div>
                     <div className = "uploadButtonEssay-subappform">
                         <div className = "uploadButtonTextDiv-subappform">
