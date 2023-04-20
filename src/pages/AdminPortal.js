89aadae63171fa061d4f8f392d786d67d27db1c8
profundities <85223114+profundities@users.noreply.github.com>
2023-04-19 22:39:06 -0700

    JIRA #165

--- src/pages/AdminPortal.js
+++ src/pages/AdminPortal.js
@@ -8,7 +8,7 @@ export function AdminPortal() {
     <div class="wrapper-admin-portal">
          
          <a href="/login"><button class="logout">Logout</button></a>
-        <p class="fs-1">
+        <p class="fs-1 adminportal-heading">
             <p class="text-center">Current Applicants</p>
             <p class="text-end">
                 <p class="fs-6">
@@ -16,9 +16,9 @@ export function AdminPortal() {
             </p>
         </p>
         <div class="row g-2">
-            <div class="col-md">
+            <div class="col-md col-md-admin">
                 <div class="form-floating form-floating-custom">
-                    <select class="form-select" id="floatingSelect" aria-label="Filter drop down menu">
+                    <select class="form-select form-admin" id="floatingSelect" aria-label="Filter drop down menu">
                         <option selected>Filter by grant, date, read/unread</option>
                         <option value="grant">grant 1</option>
                         <option value="grant2">grant 2</option>
@@ -29,8 +29,8 @@ export function AdminPortal() {
                 </div>
             </div>
         
-            <div class="col-md">
-                <div class="mb-3 search-bar">
+            <div class="col-md col-md-admin">
+                <div class="mb-3 search-bar search-admin">
                     <div class="searchField">
                         <input type="name" class="form-control" id="searchBar" placeholder="Search for applicants"></input> 
                         <button class="search">Search</button>
@@ -42,9 +42,41 @@ export function AdminPortal() {
             
         </div>
         <div class="submissions-box">
-                <button class="sub1"><a class="submission-link" href="/adminsubappform">Submission</a></button>
-                <button class="sub2"><a class="submission-link" href="/adminsubappform">Submission</a></button>
-                <button class="sub3"><a class="submission-link" href="/adminsubappform">Submission</a></button>
+                <button class="sub1"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
+
+                <button class="sub2"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
+                
+                <button class="sub3"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
+
+                <button class="sub4"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
+
+                <button class="sub5"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
+
+                <button class="sub6"><a class="submission-link" href="/adminsubappform">Submission</a> 
+                </button>
+                <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span></label>
             </div>
     </div>
     )
